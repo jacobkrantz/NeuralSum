bcb115fcd583955a9ef17105ba6465d8dffff5a3
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2018-07-10 13:56:25 -0600

    Use Fire for evaluate script

--- evaluate_on_duc.py
+++ evaluate_on_duc.py
@@ -3,8 +3,9 @@ from __future__ import print_function
 from NeuralSum import Vert
 from config import config
 
+import fire
 import logging as log
-import sys
+
 """
 script for evaluating the tensor2tensor generated summaries that are located
     in the output location specified in the decoding shell script.
@@ -13,33 +14,38 @@ script for evaluating the tensor2tensor generated summaries that are located
 - outputs score report to file as specified in config or command line argument.
 - uses about 7GB of memory. Could be reduced by half, but major rework
     of NeuralSum.Evaluation would have to be done.
-- specify whether DUC2003 or DUC2004 is used
-To run:
->>> python evaluate_on_duc.py
-To override whether or not a score report is saved:
->>> python evaluate_on_duc.py save
->>> python evaluate_on_duc.py toss
+Usage:
+    >>> python evaluate_on_duc.py [--which_duc=(2003|2004)] [--save_report=(True|False)]
+Requirements:
+    generated.txt and summaries.txt in respective DUC folder.
 """
 
-DUC = 'duc2004'
-GENERATED = './data/' + DUC + '/generated.txt'
-SUMMARIES = './data/' + DUC + '/summaries.txt'
+def evaluate(which_duc='2003', save_report=True):
+    """
+    Args:
+        which_duc (string): either 2003 or 2004. Default: 2003.
+        save_report (bool): if True, saves report to location specified in
+            the config. Default: True.
+    Returns:
+        dict of scores
+    """
+    generated_f = './data/' + which_duc + '/generated.txt'
+    summaries_f = './data/' + which_duc + '/summaries.txt'
 
-def main(save_scores):
     log.info("Starting step: read data files.")
     generated = list()
 
     try:
-        with open(GENERATED, mode='r') as gen_f:
+        with open(generated_f, mode='r') as gen_f:
             for line in gen_f:
                 generated.append(line.strip('\n'))
     except IOError as e:
-        print(e)
-        print('Try running `python process_duc.py <duc_year>` to load the data')
+        raise IOError(str(e) + '\n' +
+            'Try running `python process_duc.py <duc_year>` to load the data')
 
     summaries = list()
     try:
-        with open(SUMMARIES, mode='r') as summ_f:
+        with open(summaries_f, mode='r') as summ_f:
             for line in summ_f:
                 summaries.append(line.strip('\n'))
     except IOError as e:
@@ -58,21 +64,11 @@ def main(save_scores):
         verbose=True
     )
     vert.display_scores(scores)
-    if save_scores:
+    if save_report:
         vert.output_report(scores, config['vert']['reports_folder'])
     log.info("Finished step: Evaluate summary pairs.")
+    return scores
 
 if __name__ == '__main__':
     log.getLogger()
-    if len(sys.argv) > 3:
-        raise ValueError("Argument count too high.")
-    if len(sys.argv) == 2:
-        if sys.argv[1] == 'save':
-            save_scores = True
-        elif sys.argv[1] == 'toss':
-            save_scores = False
-        else:
-            raise ValueError("Argument must be either 'save' or 'toss'")
-        main(save_scores)
-    else:
-        main(config['vert']['output_report'])
+    fire.Fire(evaluate)
